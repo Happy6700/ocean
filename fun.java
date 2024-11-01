ecc33a85da6d568c8e0e0b7a38853e7d6a2a430c
Happy <manncovey@gmail.com>
2024-10-31 20:16:03 -0600

    FunOcean

---
+++ fun.java
@@ -0,0 +1,18 @@
+/** Madison Covey
+ * october 31rst 2024
+ */
+public class fun {
+
+    // interface
+    interface Ocean
+     public void oceanWaves();// interface method(lots of fish)
+          public void surf();// interface method(lots of fish)
+}
+// Shark "implements" the Ocean interface
+class Shark implements Ocean{
+    public void oceanWaves() {
+
+    }
+        
+    }
+}
